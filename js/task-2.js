0ca8c0afa58cc9969517920f293a3effcd1552c8
Olenalr2826 <lr2826@ukr.net>
2024-10-01 21:41:13 +0300

    add files hw2

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+function formatMessage(message, maxLenght) {
+    const message_Lenght = message.length;
+    if(message_Lenght <= maxLenght) {return message}
+    else {return message.slice(0, maxLenght) + "..."}; 
+}
+
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+console.log(formatMessage("aaaaaaaaaaaaaaaaaaa", 4));
